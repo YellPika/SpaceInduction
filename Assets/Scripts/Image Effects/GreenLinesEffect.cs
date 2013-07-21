4bd576a5850c9494e1a4101502fbd503cb6290ac
YellPika <yellpika@gmail.com>
2013-07-21 16:25:27 -0400

    Added noise to the green lines effect (which will be renamed soon). This will be linked to the player's power level.

--- Assets/Scripts/Image Effects/GreenLinesEffect.cs
+++ Assets/Scripts/Image Effects/GreenLinesEffect.cs
@@ -1,5 +1,5 @@
+using System.Linq;
 using UnityEngine;
-using System.Collections;
 
 [RequireComponent(typeof(Camera))]
 [ExecuteInEditMode]
@@ -7,26 +7,56 @@ public sealed class GreenLinesEffect : MonoBehaviour
 {
     [SerializeField]
     private Shader shader;
+    private Texture2D noiseTexture;
+
     private Material material;
     private float offset;
 
     public float Period = 10;
     public float Speed = 10;
-    public float Intensity = 0.5f;
     public Color Color = new Color(0, 0.5f, 0);
+
+    public float NoiseIntensity = 0.05f;
+    public float LineIntensity = 0.1f;
 
     private void Awake()
     {
+        var data = Enumerable
+            .Range(0, 512)
+            .Select(n => (byte)n)
+            .ToArray();
+        
+        for (int i = 0; i < data.Length; i++)
+        {
+            var index = Random.Range(i, data.Length - 1);
+
+            var temp = data[index];
+            data[index] = data[i];
+            data[i] = temp;
+        }
+
+        noiseTexture = new Texture2D(data.Length, 1, TextureFormat.ARGB32, false, true);
+        noiseTexture.SetPixels32(data
+            .Select(n => new Color32(n, n, n, n))
+            .ToArray());
+        noiseTexture.Apply();
+
+        noiseTexture.filterMode = FilterMode.Point;
+        noiseTexture.wrapMode = TextureWrapMode.Repeat;
+
         material = new Material(shader);
     }
 
     private void Update()
     {
         offset += Speed * Time.deltaTime;
+
         material.SetFloat("_Offset", offset);
         material.SetFloat("_Period", Period);
-        material.SetFloat("_Intensity", Intensity);
         material.SetColor("_Color", Color);
+        material.SetFloat("_LineIntensity", LineIntensity);
+        material.SetFloat("_NoiseIntensity", NoiseIntensity);
+        material.SetTexture("_NoiseTex", noiseTexture);
     }
 
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
--- Assets/Shaders/GreenLines.shader
+++ Assets/Shaders/GreenLines.shader
@@ -1,6 +1,7 @@
 Shader "Custom/GreenLines" {
 	Properties {
 		_MainTex ("Base (RGB)", 2D) = "" {}
+		_NoiseTex ("Noise Texture", 2D) = "" {}
 	}
 	
 	CGINCLUDE
@@ -15,10 +16,15 @@ Shader "Custom/GreenLines" {
 	sampler2D _MainTex;
 	float4 _MainTex_TexelSize;
 
+	sampler2D _NoiseTex;
+	float4 _NoiseTex_TexelSize;
+
 	float _Offset;
 	float _Period;
-	float _Intensity;
 	fixed4 _Color;
+
+	float _LineIntensity;
+	float _NoiseIntensity;
 
 	v2f vert(appdata_img v) {
 		v2f o;
@@ -29,9 +35,17 @@ Shader "Custom/GreenLines" {
 	} 
 	
 	half4 frag(v2f i) : COLOR {
-		half4 color = tex2D (_MainTex, i.uv);
+		int2 coords = i.uv.xy / _MainTex_TexelSize.xy;
+
+		half4 color = tex2D(_MainTex, i.uv);
 		int y = i.uv.y / _MainTex_TexelSize.y;
-		return lerp(color, _Color, frac((y + (int)_Offset) / _Period) * _Intensity);
+		float4 lines = lerp(color, _Color, frac((y + (int)_Offset) / _Period) * _LineIntensity);
+
+		float noise = _Offset;
+		noise = tex2D(_NoiseTex, (float2)(coords.x / 255.0f + noise)).x;
+		noise = tex2D(_NoiseTex, (float2)(coords.y / 255.0f + noise)).x;
+
+		return lerp(lines, noise, _NoiseIntensity);
 	}
 
 	ENDCG 
