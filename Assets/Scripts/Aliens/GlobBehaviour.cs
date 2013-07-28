6cb13e2d551c985a01120ecf7dd4de795bd910bd
YellPika <yellpika@gmail.com>
2013-07-28 16:48:56 -0400

    Box is now more reliable. Player now uses adaptive tone mapping. Alien code now tracks targets manually (this might break something) (was done to fix a strange bug). Level 6 is simpler (was too complex for first level involving boxes).

--- Assets/Scripts/Aliens/GlobBehaviour.cs
+++ Assets/Scripts/Aliens/GlobBehaviour.cs
@@ -1,31 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public sealed class GlobBehaviour : MonoBehaviour
 {
     [SerializeField]
-    private GlobWaypoint[] targets;
+    private Transform[] targets;
 
-    private int currentTarget;
-    private NavMeshAgent agent;
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private float turnSpeed;
+
+    [SerializeField]
+    private float tolerance;
 
     private Vector3 initialPosition;
     private Quaternion initialRotation;
 
+    private IEnumerator<Vector3> travelPath;
+
     private void Awake()
     {
-        agent = GetComponent<NavMeshAgent>();
-
         initialPosition = transform.position;
         initialRotation = transform.rotation;
     }
 
     private void Start()
     {
-        if (targets == null || targets.Length == 0)
-            return;
-
-        currentTarget = 0;
-        agent.SetDestination(targets[currentTarget].transform.position);
+        travelPath = TravelPath();
     }
 
     private void Restart()
@@ -38,13 +42,49 @@ public sealed class GlobBehaviour : MonoBehaviour
 
     private void Update()
     {
-        if (targets == null || targets.Length == 0)
-            return;
+        travelPath.MoveNext();
 
-        if (Vector3.Distance(transform.position, targets[currentTarget].transform.position) < targets[currentTarget].Tolerance)
+        var source = transform.rotation.eulerAngles;
+        var target = Quaternion.LookRotation(travelPath.Current - transform.position).eulerAngles;
+
+        transform.rotation = Quaternion.Euler(
+            Mathf.MoveTowardsAngle(source.x, target.x, turnSpeed * Time.deltaTime),
+            Mathf.MoveTowardsAngle(source.y, target.y, turnSpeed * Time.deltaTime),
+            Mathf.MoveTowardsAngle(source.z, target.z, turnSpeed * Time.deltaTime));
+    }
+
+    private IEnumerator<Vector3> TravelPath()
+    {
+        var currentTarget = 0;
+        var path = new NavMeshPath();
+        var corners = new Queue<Vector3>();
+
+        while (true)
         {
-            currentTarget = (currentTarget + 1) % targets.Length;
-            agent.SetDestination(targets[currentTarget].transform.position);
+            if (corners.Count == 0)
+            {
+                currentTarget = (currentTarget + 1) % targets.Length;
+                NavMesh.CalculatePath(transform.position, targets[currentTarget].transform.position, -1, path);
+
+                foreach (var corner in path.corners)
+                    corners.Enqueue(corner);
+            }
+
+            var target = corners.Peek();
+
+            var direction = target - transform.position;
+            var distance = direction.magnitude;
+
+            if (distance < speed * Time.deltaTime)
+                transform.position = target;
+            else
+            {
+                transform.position += direction.normalized * speed * Time.deltaTime;
+                yield return target;
+            }
+
+            if (Vector3.Distance(transform.position, target) < tolerance)
+                corners.Dequeue();
         }
     }
 }
--- Assets/Scripts/Boxes/MovableBox.cs
+++ Assets/Scripts/Boxes/MovableBox.cs
@@ -6,6 +6,12 @@ public sealed class MovableBox : MonoBehaviour
 {
     private Vector3 startPosition;
 
+    [SerializeField]
+    private PhysicMaterial smoothMaterial;
+
+    [SerializeField]
+    private PhysicMaterial roughMaterial;
+    
     private void Awake()
     {
         startPosition = transform.position;
@@ -17,14 +23,26 @@ public sealed class MovableBox : MonoBehaviour
         if (mover == null)
             return;
 
+        rigidbody.mass = mover.IsActivated ? 0.1f : 10;
+        collider.material = mover.IsActivated ? smoothMaterial : roughMaterial;
+
         if (mover.IsActivated)
         {
             var offset = mover.transform.position - transform.position;
 
             if (Mathf.Abs(offset.x) > Mathf.Abs(offset.z))
+            {
                 rigidbody.velocity = new Vector3(mover.rigidbody.velocity.x, 0, 0);
+                rigidbody.constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionZ;
+            }
             else
+            {
                 rigidbody.velocity = new Vector3(0, 0, mover.rigidbody.velocity.z);
+                rigidbody.constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionX;
+            }
+
+            // to prevent the box from moving slower than the player.
+            rigidbody.velocity *= 1.25f;
         }
     }
 
